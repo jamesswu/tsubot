562fcebf22a44ce17322358f33f02b80d4a9852a
James Wu <wu.james@hotmail.com>
2022-08-26 15:31:33 -0400

    changes

--- src/index.ts
+++ src/index.ts
@@ -9,4 +9,18 @@ client.once('ready', () => {
   console.log('tsubot is ready');
 });
 
+client.on('interactionCreate', async interaction => {
+  if (!interaction.isCommand()) return;
+
+  const { commandName } = interaction;
+
+  if (commandName === 'ping') {
+    await interaction.reply('Pong!');
+  } else if (commandName === 'server') {
+    await interaction.reply('Server info.');
+  } else if (commandName === 'user') {
+    await interaction.reply('User info.');
+  }
+});
+
 client.login(process.env.TOKEN!);
