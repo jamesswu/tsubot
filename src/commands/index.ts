5eeecf9b1cf54eef9e842aa5e3985c54b189c4bb
James Wu <wu.james@hotmail.com>
2022-11-02 00:42:50 -0400

    commands

---
+++ src/commands/index.ts
@@ -0,0 +1,2 @@
+// export * as <command> from './<command>
+export * as ping from './ping';
--- src/deploy-commands.ts
+++ src/deploy-commands.ts
@@ -1,22 +1,20 @@
-import { SlashCommandBuilder } from '@discordjs/builders';
 import { REST } from '@discordjs/rest';
 import { Routes } from 'discord-api-types/v10';
-import { config } from 'dotenv'
-config();
+import config from './config'
+import * as commandModules from './commands';
 
-const TOKEN: string = process.env.TOKEN!;
-const CLIENT_ID: string = process.env.CLIENT_ID!;
-const GUILD_ID: string = process.env.GUILD_ID!;
+type Command = {
+  data: unknown
+}
 
+const commands = [];
 
-const commands = [
-  new SlashCommandBuilder().setName('ping').setDescription('Replies with pong!'),
-  new SlashCommandBuilder().setName('server').setDescription('replies with server info!'),
-  new SlashCommandBuilder().setName('user').setDescription('replies with user info!'),
-].map(command => command.toJSON());
+for (const module of Object.values<Command>(commandModules)) {
+  commands.push(module.data);
+}
 
-const rest = new REST({ version: '9' }).setToken(TOKEN);
+const rest = new REST({ version: '9' }).setToken(config.DISCORD_TOKEN);
 
-rest.put(Routes.applicationGuildCommands(CLIENT_ID, GUILD_ID), { body: commands })
+rest.put(Routes.applicationGuildCommands(config.CLIENT_ID, config.GUILD_ID), { body: commands })
   .then(() => console.log('Successfully registered application commands.'))
   .catch(console.error);
