9bb28ddc3cb9690227623a95a29a2076d54fff95
James Wu <wu.james@hotmail.com>
2023-08-11 20:47:47 -0400

    cleanup

--- src/commands/pf.ts
+++ src/commands/pf.ts
@@ -1,8 +1,6 @@
 import { Embed, SlashCommandBuilder } from 'discord.js'
 import type { CommandInteraction } from 'discord.js';
-import { EmbedBuilderWrapper } from '../utils/embed';
 import axios from 'axios';
-
 
 const data = new SlashCommandBuilder()
   .setName("pf")
@@ -20,10 +18,24 @@ const data = new SlashCommandBuilder()
 const execute = async (interaction:any) => {
   const duty = interaction.options.getString('duty');
   
-  const response = await axios.get(`http://127.0.0.1:8000/${duty}`);
-  const listings = response.data
-  const embed = EmbedBuilderWrapper(listings);
-  return await interaction.reply({embeds: [embed]});
+  const response = await axios.get(`http://127.0.0.1:8080/listings/${duty}`);
+  // console.log(response.data)
+  const listings = response.data.data
+
+  let fields = []
+
+  for (const i in listings) {
+    const temp:
+    temp.name = listings[i].creator 
+  }
+
+  const embed = {
+    title: listings[0].title
+
+  }
+
+
+  return await interaction.reply({embeds: [data]});
 }
 
 export {
