912412ac60876cf2a9fbb076d25fbfa64a44a1f8
James Wu <wu.james@hotmail.com>
2023-08-29 19:59:00 -0400

    embed formating

--- src/commands/pf.ts
+++ src/commands/pf.ts
@@ -25,7 +25,7 @@ const execute = async (interaction:ChatInputCommandInteraction) => {
   const listings = response.data.data
 
   if (listings && listings.length) {
-    const embedArray = []
+    const embedsArray = []
     // go through the listings array
     let fieldsArray = []
     for (let i = 0; i < listings.length; i++) {
@@ -38,38 +38,79 @@ const execute = async (interaction:ChatInputCommandInteraction) => {
           party += RoleHandler(listings[i].party[j].Roles.Roles) + ' '
         }
       }
-      let col1 = { name: listings[i].creator, value: party, inline: true }
-      let col2 = { name: listings[i].tags, value: listings[i].description, inline: true }
-      let col3 = { name: listings[i].expires, value: listings[i].updated, inline: true }
+      const col1 = { name: listings[i].creator, value: party, inline: true }
+      const col2 = { name: listings[i].tags, value: listings[i].description, inline: true }
+      const col3 = { name: `<:timer:1146224804748333118>${listings[i].expires}`, value: `<:hourglass:1146224937581949020>${listings[i].updated}`, inline: true }
       fieldsArray.push(col1)
       fieldsArray.push(col2)
       fieldsArray.push(col3)
 
       // constructs an embed and pushes it onto the array 
       if ((i+1)%5 == 0) {
-        const embed = {
-          title: listings[0].duty,
-          fields: fieldsArray,
-          footer: {
-            text: `${listings[0].datacenter} data center`
+        // add title if this is the first embed being pushed
+        if (embedsArray.length == 0 && i == listings.length-1) {
+          const embed = {
+            title: listings[0].duty,
+            fields: fieldsArray,
+            footer: {
+              text: `${listings[0].datacenter} Data Center`
+            }
           }
+          fieldsArray = []
+          embedsArray.push(embed)
+        // handles the where the embed being pushed is the first and not the last one
+        } else if (embedsArray.length == 0) {
+          const embed = {
+            title: listings[0].duty,
+            fields: fieldsArray,
+          }
+          fieldsArray = []
+          embedsArray.push(embed)
+          
+        } else if (i == listings.length-1) {
+          const embed = {
+            fields: fieldsArray,
+            footer: {
+              text: `${listings[0].datacenter} Data Center`
+            }
+          }
+          fieldsArray = []
+          embedsArray.push(embed)
+        // handles the case where the embed being pushed is not the first and not the last
+        } else {
+          const embed = {
+            fields: fieldsArray,
+          }
+          fieldsArray = []
+          embedsArray.push(embed)
         }
-        fieldsArray = []
-        embedArray.push(embed)
       // constructs an embed of remaining listings that have not been pushed to the array
       } else if (i == listings.length-1) {
-        const embed = {
-          title: listings[0].duty,
-          fields: fieldsArray,
-          footer: {
-            text: `${listings[0].datacenter} data center`
+        // handles the case where it is the first and last embed
+        if (embedsArray.length == 0) {
+          const embed = {
+            title: listings[0].duty,
+            fields: fieldsArray,
+            footer: {
+              text: `${listings[0].datacenter} Data Center`
+            }
           }
+          fieldsArray = []
+          embedsArray.push(embed)
+        // handles the case where this is the last embed
+        } else {
+          const embed = {
+            fields: fieldsArray,
+            footer: {
+              text: `${listings[0].datacenter} Data Center`
+            }
+          }
+          fieldsArray = []
+          embedsArray.push(embed)
         }
-        fieldsArray = []
-        embedArray.push(embed)
       }
     }
-    return await interaction.reply({embeds: embedArray, ephemeral: true});
+    return await interaction.reply({embeds: embedsArray, ephemeral: true});
   }
 }
 
