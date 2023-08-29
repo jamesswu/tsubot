f27fa3af77d2aea7a9c620e9105cf7bef4ce8090
James Wu <wu.james@hotmail.com>
2023-08-29 18:30:50 -0400

    decoupling deployment for global and guild commands

--- src/deploy-global.ts
+++ src/deploy-global.ts
@@ -18,10 +18,8 @@ const rest = new REST().setToken(config.DISCORD_TOKEN);
 (async () => {
   try {
     console.log(`Started refreshing ${commands.length} application (/) commands.`);
-
-    const data: any = await rest.put(Routes.applicationGuildCommands(config.CLIENT_ID, config.GUILD_ID), { body: commands });
-    console.log(`Successfully reloaded ${data.length} application (/) dev commands.`);
-
+    const data: any = await rest.put(Routes.applicationCommands(config.CLIENT_ID), {body: commands})
+    console.log(`Successfully reloaded ${data.length} application (/) global commands.`);
   } catch (error) {
     console.log(error);
   }
